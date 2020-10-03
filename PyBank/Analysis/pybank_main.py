45c70130333515464c57bb2d420f5171336f606c
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2020-10-03 10:14:20 -0400

    75% complete

---
+++ PyBank/Analysis/pybank_main.py
@@ -0,0 +1,60 @@
+
+#library imports
+import os  
+import csv
+
+#set filepath 
+filepath = os.path.join("Resource","budget_data.csv")
+print(filepath)
+outputfilepath = os.path.join("Analysis","analysis.txt")
+
+#open and read file
+with open(filepath) as file: 
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+
+    #set variables
+    total_months = 0
+    total = 0
+    average = 0
+    profit_list = []
+    difference_list = []
+    highest_profit = []
+    lowest_profit = []
+
+    #create profit and difference list
+    for row in csvreader:
+        
+        total_months = total_months +1
+        total = total + int(row[1])
+        profit_list.append(row[1])
+    
+    for profit in range(len(profit_list)):
+        if profit == 0:
+            continue
+        else:
+            difference_list.append(int(profit_list[profit]) - int(profit_list[profit - 1]))
+
+    #calculate summary variables
+    average = sum(difference_list)/len(difference_list)
+    highest_profit = max(difference_list)
+    lowest_profit = min(difference_list)
+
+    #print summary variables in terminal
+    print(f"Total Months: {total_months}")
+    print(f"Total: ${total}")
+    print(f"Average Change: ${average:.2f}")
+    print(f"Greatest Increase in Profits : ${highest_profit}")
+    print(f"Greatest Decrease in Profits : ${lowest_profit}")
+
+    
+#open output file
+with open(outputfilepath, 'w') as outputfile:
+    csvwriter = csv.writer(outputfile)
+
+    #print summary variables in txt file
+    print(f"Total Months: {total_months}")
+    print(f"Total: ${total}")
+    print(f"Average Change: ${average:.2f}")
+    print(f"Greatest Increase in Profits : ${highest_profit}")
+    print(f"Greatest Decrease in Profits : ${lowest_profit}")
---
+++ PyPoll/Analysis/pypoll_main.py
@@ -0,0 +1,56 @@
+
+#library imports
+import os  
+import csv
+
+#set filepath 
+filepath = os.path.join("Resource","election_data.csv")
+print(filepath)
+outputfilepath = os.path.join("Analysis","analysis.txt")
+
+#open and read file
+with open(filepath) as file: 
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+
+    #set variables
+    total_votes = 0
+    total_candidates = []
+    candidate_votes = 0
+    candidate_list = []
+    votes_list = []
+    percent = 0
+    percent_votes = []
+
+    #create candidate list
+    for row in csvreader:
+        
+        total_votes = total_votes + 1
+        total_candidates.append(row[2])
+
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+
+    #calculate votes for each candidate
+    for i in range(len(candidate_list)):
+        for j in range(len(total_candidates)):
+            if candidate_list[i] == total_candidates[j]:
+                candidate_votes = candidate_votes + 1
+        votes_list.append(candidate_votes)
+        candidate_votes = 0
+
+    #calculate percentage of votes
+    for x in range(len(votes_list)):
+        percent = 100*(votes_list[x]/sum(votes_list))
+        percent_votes.append(percent)
+
+    #print summary variables in terminal
+    print("Election Results")
+    print("---------------------")
+    print(f"Total Votes: {total_votes}")
+    print("---------------------")
+    for y in range(len(candidate_list)):
+        print(f"{candidate_list[y]}: {percent_votes[y]:.3f}% ({votes_list[y]})")
+    print("---------------------")
+    print(f"Winner: ")
+    print("---------------------")
